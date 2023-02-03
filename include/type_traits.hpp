b2da00870c9ea898ff188cbaf0630d7a76ca3511
Yoma <codeyoma@gmail.com>
2023-02-03 21:19:58 +0900

    [edit] add enable_if, enable_if_t in traits

--- include/type_traits.hpp
+++ include/type_traits.hpp
@@ -10,13 +10,27 @@ struct integral_constant {
 	static const _Tp		  value = __v;
 	typedef _Tp				  value_type;
 	typedef integral_constant type;
-
-	const value_type operator()() const { return value; }
-					 operator value_type() const { return value; }
-	// constexpr value_type	  operator()() const { return value; }
-	// constexpr				  operator value_type() const { return value; }
+							  operator value_type() const { return value; }
+	const value_type		  operator()() const { return value; }
+	// constexpr				  operator value_type() const { return value; } // c++11
+	// constexpr value_type	  operator()() const { return value; } // c++14
 };
 
+// template< class _Tp, _Tp __v >
+// const _Tp integral_constant< _Tp, __v >::value;
+
+// enable_if
+template< bool, class _Tp = void >
+struct enable_if {};
+template< class _Tp >
+struct enable_if< true, _Tp > {
+	typedef _Tp type;
+};
+
+template< bool _Bp, class _Tp = void >
+struct enable_if_t : public enable_if< _Bp, _Tp >::type {};
+
+// bool_constant
 #define _FT_BOOL_CONSTANT_(__b) integral_constant< bool, (__b) >
 typedef _FT_BOOL_CONSTANT_(true) true_type;
 typedef _FT_BOOL_CONSTANT_(false) false_type;
